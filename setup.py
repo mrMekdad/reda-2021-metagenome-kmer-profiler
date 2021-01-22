e23e973d06e2e752106c9ff0b7485cbaf18efdb2
mrMekdad <mekdadr6@gmail.com>
2021-01-22 23:19:00 +0100

    metagenome-kmer-profiler: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2021-metagenome-kmer-profiler",
+    version="0.1.0",
+    description="Exploration project for k-mer profiles and sample-level comparative summaries.",
+    author="Red@",
+    packages=["metagenome_kmer_profiler"],
+    package_dir={"": "src"},
+)
